af579a4a3a22cecac082e57b69bddd47c2483416
youssefdrif <93372842+youssefdrif@users.noreply.github.com>
2023-06-13 16:28:30 +0200

    firebase installation

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: 'AIzaSyD09wHAmCi7LUaMfzTT70OOp_PWZVe38jE',
+      authDomain: 'ecommerce-d176d.firebaseapp.com',
+      projectId: 'ecommerce-d176d',
+      storageBucket: "ecommerce-d176d.appspot.com",
+      messagingSenderId: "97641437241",
+      appId: "1:97641437241:web:e707975a67530a384515e8",
+      measurementId: "G-8ED7974766"
+    }
+  };
+  
